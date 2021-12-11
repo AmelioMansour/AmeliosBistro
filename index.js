9d250619d1f9a32fd9304d1f9085458ebba8a617
AmelioMansourPC <ameliomansour@yahoo.com>
2021-12-11 15:35:00 -0500

    did some work on pages

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const ejs = require("ejs");
 const { request } = require("express");
 
+
 const app = express();
 
 
@@ -15,6 +16,18 @@ app.use("/public", express.static(__dirname + "/public"));
 //Nav routes
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+app.get("/views/index.ejs", (req, res) => {
+    res.render("index");
+});
+
+app.get("/views/html/about.ejs", (req, res) => {
+    res.render("./html/about.ejs");
+});
+
+app.get("/views/html/menu.ejs", (req, res) => {
+    res.render("./html/menu.ejs");
 });
 
 
