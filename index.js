d541440b08f7d8eed6a76a0e1291a16e46d97e27
AmelioMansourPC <ameliomansour@yahoo.com>
2021-12-10 22:58:36 -0500

    fixed original node stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const { request } = require("express");
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+//Nav routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
